a4c1e6f881821c265329a7d68defb202f5af5132
isaac kamel <isaackamel12345@gmail.com>
2025-04-17 10:10:37 +0300

    new commit

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -1,4 +1,5 @@
 exports.home = (req, res, next) => {
+  // console.log(req.session);
     res.render("layouts/parent");
     //
   };
--- middlewares/guest.js
+++ middlewares/guest.js
@@ -1,8 +1,8 @@
 exports.guest = (req, res, next) => {
-    if ("isAuthenticated" in req.session) {
-        if (req.session.isAuthenticated) {
-        res.redirect("/cms");//ازا كان اوثونتيكيتد وديه على الهوم وازا لا رجعو 
-        }
-      }
-     next();
-}
+  if ("isAuthenticated" in req.session) {
+    if (req.session.isAuthenticated) {
+      return res.redirect("/cms"); //ازا كان اوثونتيكيتد وديه على الهوم وازا لا رجعو
+    }
+  }
+  next();
+};
